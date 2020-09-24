141a1c530b73902485e093fc1e7b3209b4f8b1eb
Kanutsanun Bouking <52057321+kanutsanun-b@users.noreply.github.com>
2020-09-24 16:24:40 +0700

    Add files via upload

--- nsbotRaspi/__init__.py
+++ nsbotRaspi/__init__.py
@@ -1,2 +1,1 @@
 from nsbotRaspi.nspi import MetarSpeciTaf
-from nsbotRaspi.nspi import LoopNsweb
--- nsbotRaspi/nspi.py
+++ nsbotRaspi/nspi.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from time import sleep
 import schedule
 import datetime
@@ -10,78 +9,113 @@ class MetarSpeciTaf:
         self.metarspeci_target = "https://nsweb.tmd.go.th/#showMetars"
         self.taf_target = "https://nsweb.tmd.go.th/#showTAFs"
         self.line_token = line_token
-        self.time_stop = time_stop #UTC Time
-        self.code_metarspeci = 'initial code'
-        self.code_taf = 'initial code'
+        self.time_stop = time_stop # UTC Time
+        self.code_metarspeci = 'initial_code'
+        self.code_taf = 'initial_code'
         self.hr = 'time'
         
     def run_MetarSpeci(self):
-        try:
-            options = webdriver.ChromeOptions()
-            options.headless = True
-            driver = webdriver.Chrome(options=options)
-            driver.get(self.metarspeci_target);
-            sleep(15)
-            list_of_elements = driver.find_elements_by_xpath('//p[@class="js-metar"]')
-            stations_metarspeci = [station.text for station in list_of_elements];
-            find_vtse_metarspeci = [i for i in stations_metarspeci if "VTSE" in i]
-            read_metarspeci = find_vtse_metarspeci[0]
-            driver.quit()
         
-            if read_metarspeci == self.code_metarspeci:
-                pass
-            else:
-                self.code_metarspeci = read_metarspeci
-                url = 'https://notify-api.line.me/api/notify'
-                token = self.line_token
-                headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
-                msg = self.code_metarspeci
-                r = requests.post(url, headers=headers , data = {'message':msg})        
-                print(self.code_metarspeci)
-
-            dt = datetime.datetime.utcnow()
-            self.hr = dt.strftime('%H')
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver_metarspeci = webdriver.Chrome(options=options)
+        driver_metarspeci.get(self.metarspeci_target);
+        sleep(10)
+        
+        while self.hr != self.time_stop:
             
-        except:
-            print("METAR or SPECI error")
-        
-    def run_Taf(self):
-        
-        try:
-            options = webdriver.ChromeOptions()
-            options.headless = True
-            driver = webdriver.Chrome(options=options)
-            driver.get(self.taf_target);
+            driver_metarspeci.refresh()
             sleep(10)
-            list_of_elements = driver.find_elements_by_xpath('//p[@class="js-taf"]')
-            stations_taf = [station.text for station in list_of_elements];
             
-            find_vtse_taf = [i for i in stations_taf if "VTSE" in i]
-            read_taf = find_vtse_taf[0]
-            driver.quit()
-        
-            if read_taf == self.code_taf:
-                pass
-            else:
-                self.code_taf = read_taf
-                url = 'https://notify-api.line.me/api/notify'
-                token = self.line_token
-                headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
-                msg = self.code_taf
-                r = requests.post(url, headers=headers , data = {'message':msg})        
-                print(self.code_taf)
+            try:
+                list_of_elements = driver_metarspeci.find_elements_by_xpath('//p[@class="js-metar"]')
+                stations = [station.text for station in list_of_elements];
+                find_vtse = [i for i in stations if "VTSE" in i]
+                read = find_vtse[0]
+            
+                if read == self.code_metarspeci:
+                    pass
+                else:
+                    self.code_metarspeci = read
+                    url = 'https://notify-api.line.me/api/notify'
+                    token = self.line_token
+                    headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+                    msg = self.code_metarspeci
+                    r = requests.post(url, headers=headers , data = {'message':msg})        
+                    print(self.code_metarspeci)
+
+                dt = datetime.datetime.utcnow()
+                self.hr = dt.strftime('%H')
                 
-        except:
-            print("TAF error")
+                if self.hr == self.time_stop:
+                    print('Program stopped')
+                    break
+                else:
+                    sleep(40)
+                    
+            except Exception as e:
+                
+                print("METAR or SPECI error: " ,e)
+                dt = datetime.datetime.utcnow()
+                self.hr = dt.strftime('%H')
+                
+                if self.hr == self.time_stop:
+                    print('Program stopped')
+                    break
+                else:
+                    sleep(40)
+                    
+        driver_metarspeci.quit()
+            
+    def run_Taf(self):
+                
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver_taf = webdriver.Chrome(options=options)
+        driver_taf.get(self.taf_target);
+        sleep(10)
         
-class LoopNsweb(MetarSpeciTaf):
-    
-    def __init__(self, line_token, time_stop):
-        super().__init__(line_token, time_stop)
+        while True:
+            
+            driver_taf.refresh()
+            sleep(10)
+            
+            try:
+                list_of_elements = driver_taf.find_elements_by_xpath('//p[@class="js-taf"]')
+                stations = [station.text for station in list_of_elements];
+                find_vtse = [i for i in stations if "VTSE" in i]
+                read = find_vtse[0]
+            
+                if read == self.code_taf:
+                    pass
+                else:
+                    self.code_taf = read
+                    url = 'https://notify-api.line.me/api/notify'
+                    token = self.line_token
+                    headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+                    msg = self.code_taf
+                    r = requests.post(url, headers=headers , data = {'message':msg})        
+                    print(self.code_taf)
+
+                dt = datetime.datetime.utcnow()
+                self.hr = dt.strftime('%H')
+                
+            except Exception as e:
+                dt = datetime.datetime.utcnow()
+                self.hr = dt.strftime('%H')
+                print("TAF error: ", e)
+                
+            else:
+                print('Achieved')
+                break
+            
+        driver_taf.quit()
+            
         
-    def run_loop(self):
-        schedule.every().minute.at(":05").do(self.run_MetarSpeci)
+    def loop_Taf(self):
+        
         schedule.every().hour.at(":05").do(self.run_Taf)
+        
         while self.hr != self.time_stop:
             schedule.run_pending()
             sleep(1)
